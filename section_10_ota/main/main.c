d76e5ecb71f803bfcaf1ebb8af14c2bc6c662786
ralvescosta <rafael.rac.mg@gmail.com>
2022-12-26 14:05:02 -0300

    feat: section 10

---
+++ section_10_ota/main/main.c
@@ -0,0 +1,24 @@
+/**
+ * Application entry point.
+ */
+
+#include "nvs_flash.h"
+
+#include "wifi_app.h"
+
+void app_main(void)
+{
+  // Initialize NVS
+  esp_err_t res = nvs_flash_init();
+
+  if (res == ESP_ERR_NVS_NO_FREE_PAGES || res == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    res = nvs_flash_init();
+  }
+
+  ESP_ERROR_CHECK(res);
+
+  // Start Wifi
+  wifi_app_start();
+}
